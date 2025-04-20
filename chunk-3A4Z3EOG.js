cb2ac9dd1937b40dc0da00523a49b4fc7ea747b8
huangyulong12 <61046251+huangyulong12@users.noreply.github.com>
2025-04-20 02:34:06 +0000

    Deploying to gh-pages from @ huangyulong12/nav@f17c0e1a035bc07a991672132079dc45ddc65800 🚀

---
+++ chunk-3A4Z3EOG.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-IOO2QJ4K.js";import{Fb as e,vb as r}from"./chunk-VZUWZBBR.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
